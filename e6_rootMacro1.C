85d95c04af21da84b2e63192a4af746a591f87a0
kaya <kaya@kaya-Satellite-A300>
2013-04-16 22:20:56 +0300

    "e6_rootMacro1.C" dosyası aşağıdaki özelliklere göre düzenlenecek. Bu düzenlemelerden yalnızca "        1) e+e- ve mu+mu- invariant mass histogramlarini ekleyelim" bölümünü yaptım şu ana kadar. Şimdi Z bozonu ile ilgili olan kısma geçiyorum.
    Erkcan Özcan on 16.04.2013:
            1) e+e- ve mu+mu- invariant mass histogramlarini ekleyelim, Z bozonunu gorelim.
            2) Z bozonu ile olaydaki en yuksek pt'li jeti birlestirip invariant mass histogramina bakalim.

--- e6_rootMacro1.C
+++ e6_rootMacro1.C
@@ -1,4 +1,11 @@
 /*
+ * *******************************
+ * Erkcan Özcan on 16.04.2013 :  
+ * 
+        1) e+e- ve mu+mu- invariant mass histogramlarini ekleyelim, Z bozonunu gorelim.
+        2) Z bozonu ile olaydaki en yuksek pt'li jeti birlestirip invariant mass histogramina bakalim.
+ ***************************
+ * 
  * aim of this ROOT macro
  * 3.".root" dosyası --> MakeClass
         in each "event" : 
@@ -9,6 +16,8 @@
  * 
  * I adopted this code from the following file:
  * /home/kaya/Documents/particle-physics-kaya/Delphes/Delphes-3.0.5/examples/Example1.C
+ * other refs.
+ * /home/kaya/Documents/particle-physics-kaya/Delphes/Delphes-3.0.5/examples/Example3.C
  * 
  * how to run file:
  * root -l examples/e6_rootMacro1.C\(\"delphes_output.root\"\)
@@ -16,11 +25,14 @@
 
 //#include <root/TObjArray.h>
 #include <math.h>
+//#include <root/TH1.h>
 //#include <cmath>
 #include <stdlib.h>
+//#include <root/Rtypes.h>
 //#include <root/TH1.h>
 //#include <root/TFile.h>
 
+class ExRootTreeReader;
 
 //------------------------------------------------------------------------------
 
@@ -28,11 +40,14 @@ void e6_rootMacro1(const char *inputFile) {
     gSystem->Load("libDelphes");
 
     // Create chain of root trees
-    TChain chain("Delphes");
-    chain.Add(inputFile);
+    //    TChain chain("Delphes");
+    //    chain.Add(inputFile);
+    TChain *chain = new TChain("Delphes"); // adopted from Example3.C of Delphes
+    chain->Add(inputFile);
 
     // Create object of class ExRootTreeReader
-    ExRootTreeReader *treeReader = new ExRootTreeReader(&chain);
+    //ExRootTreeReader *treeReader = new ExRootTreeReader(&chain);
+    ExRootTreeReader *treeReader = new ExRootTreeReader(chain);          // adopted from Example3.C of Delphes
     Long64_t numberOfEntries = treeReader->GetEntries();
 
 
@@ -56,14 +71,17 @@ void e6_rootMacro1(const char *inputFile) {
     Double_t maxPhi = -1;
     Double_t currentPhi = 0;
 
+    Double_t currentMass = 0;
+
     // Get pointers to branches used in this analysis
+    TClonesArray *branchParticle = treeReader->UseBranch("Particle");
     TClonesArray *branchElectron = treeReader->UseBranch("Electron");
     TClonesArray *branchMuon = treeReader->UseBranch("Muon");
     TClonesArray *branchJet = treeReader->UseBranch("Jet");
 
     // Book histograms
-    Double_t histMaxPT_upper=600000.0;
-    Double_t histMaxPT_jet_upper=1000000.0;
+    Double_t histMaxPT_upper = 600000.0;
+    Double_t histMaxPT_jet_upper = 1000000.0;
     TH1 *histMaxPT_electron = new TH1F("MaxPT_electron", "maximum of electron P_{T}", 100, 0.0, histMaxPT_upper);
     TH1 *histMaxEta_electron = new TH1F("MaxPT_Eta", "maximum of electron abs(Eta)", 100, 0.0, 4);
     TH1 *histMaxPhi_electron = new TH1F("MaxPT_Phi", "maximum of electron abs(Phi)", 100, 0.0, 4);
@@ -71,6 +89,16 @@ void e6_rootMacro1(const char *inputFile) {
     TH1 *histMaxPT_jet = new TH1F("MaxPT_jet", "maximum of jet P_{T}", 100, 0.0, histMaxPT_jet_upper);
     TH1 *hist2ndMaxPT_jet = new TH1F("Max2ndPT_jet", "2nd highest of jet P_{T}", 100, 0.0, histMaxPT_jet_upper);
 
+    //  1) e+e- ve mu+mu- invariant mass histogramlarini ekleyelim, Z bozonunu gorelim.
+    //  1) e+e- ve mu+mu- invariant mass histogramlarini ekleyelim, 
+    Double_t histMassElectron_upper = 0.001;
+    Double_t histMassMuon_upper = 0.2;
+    TH1 *histMass_electron = new TH1F("Mass_electron", "mass of electron (e+e-)", 100, 0.0, histMassElectron_upper);
+    TH1 *histMass_muon = new TH1F("Mass_muon", "mass of muon (mu+mu-)", 100, 0.0, histMassMuon_upper);
+    // Z bozonunu gorelim.
+    
+
+    GenParticle *particle;
     Electron *electron;
     Muon *muon;
     Jet *jet;
@@ -94,6 +122,8 @@ void e6_rootMacro1(const char *inputFile) {
         for (int i = 0; i < numElectrons; i++) {
             electron = (Electron *) branchElectron->At(i);
             currentPT = electron->PT;
+            particle = (GenParticle*) electron->Particle.GetObject();
+            currentMass = particle->Mass;
             // http://www.cplusplus.com/reference/cmath/fabs/
             currentEta = fabs(electron->Eta);
             currentPhi = fabs(electron->Phi);
@@ -108,6 +138,7 @@ void e6_rootMacro1(const char *inputFile) {
             if (maxPhi < currentPhi) {
                 maxPhi = currentPhi;
             }
+            histMass_muon->Fill(currentMass);
         }
         histMaxPT_electron->Fill(maxPT);
         histMaxEta_electron->Fill(maxEta);
@@ -119,10 +150,13 @@ void e6_rootMacro1(const char *inputFile) {
         for (int i = 0; i < numMuons; i++) {
             muon = (Muon *) branchMuon->At(i);
             currentPT = muon->PT;
+            particle = (GenParticle*) muon->Particle.GetObject();
+            currentMass = particle->Mass;
 
             if (maxPT < currentPT) {
                 maxPT = currentPT;
             }
+            histMass_muon->Fill(currentMass);
         }
         histMaxPT_muon->Fill(maxPT);
 
@@ -156,4 +190,7 @@ void e6_rootMacro1(const char *inputFile) {
      */
 
     f.Write();
+
+    delete treeReader;
+    delete chain;
 }
