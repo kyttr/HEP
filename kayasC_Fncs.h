8a8055403bc500ff76424f17e974f809cc472d40
kaya <kaya@kaya-Satellite-A300>
2013-04-23 17:24:56 +0300

    Bu proje için gerekli, ancak proje dışında da kullanılabilecek C/C++ metotları.

---
+++ kayasC_Fncs.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   kayasC_Fncs.h
+ * Author: kaya
+ *
+ * Created on April 23, 2013, 5:22 PM
+ */
+
+#ifndef KAYASC_FNCS_H
+#define	KAYASC_FNCS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* KAYASC_FNCS_H */
+
